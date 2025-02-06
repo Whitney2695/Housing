b7d7926ca5acd580d5906469d4ccd730087f466e
Whitney <whitney2695@example.com>
2025-02-06 19:18:21 +0300

    user

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './userdashboard/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { UserdashboardComponent} from './userdashboard/userdashboard/userdashboard.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'register', component: RegisterComponent}
+    { path: 'register', component: RegisterComponent},
+    { path: 'user', component: UserdashboardComponent}
 ];
